eef48388cbe181a7d402d287bf9291ce2ddecaab
Josh Espinoza <joshespi@gmail.com>
2022-10-01 23:13:00 -0600

    created check to switch the post title heading and add or remove the link to the post.

--- espi-master-theme/index.php
+++ espi-master-theme/index.php
@@ -39,8 +39,18 @@
       ?>
         <article class="post">
           <header class="article-header">
-            <h1><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h1>
             <?php the_post_thumbnail('large', array('class' => 'featured')); ?>
+            <?php
+            if (is_singular()) {
+            ?>
+              <h1><?php the_title() ?></h1>
+            <?php
+            } else {
+            ?>
+              <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+            <?php
+            }
+            ?>
             <ul>
               <li><?php the_author(); ?></li>
               <li><?php the_date(); ?></li>
